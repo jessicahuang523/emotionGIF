9fc7b2ca81f843723bec7895436593dd5ef4b469
yungchieh <jessicahuang29@gmail.com>
2020-07-29 14:04:23 +0800

    make page done

--- get_gif.py
+++ get_gif.py
@@ -2,38 +2,41 @@ import json
 import requests
 import urllib.request
 
-tenor = []
-giphy = []
+def get_gif_url (name, cat):
 
-#tenor API
-# set the apikey and limit
-apikey = "KOBZBCHIOB9T" 
-lmt = 5
+    tenor = []
+    giphy = []
 
-search_term = "agree"
+    #tenor API
+    # set the apikey and limit
+    apikey = "KOBZBCHIOB9T" 
+    lmt = 5
 
-# get the top 8 GIFs for the search term
-r = requests.get(
-    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
+    search_term = cat
 
-if r.status_code == 200:
-    # load the GIFs using the urls for the smaller GIF sizes
-    top_5gifs = json.loads(r.content)
-else:
-    top_5gifs = None
+    # get the top 8 GIFs for the search term
+    r = requests.get(
+        "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
 
-# extract tenor GIF
-for GIFs in top_5gifs['results']:
-    tenor.append(GIFs['media'][0]['tinygif']['url'])
+    if r.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        top_5gifs = json.loads(r.content)
+    else:
+        top_5gifs = None
 
-#giphy api
-data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=agree&api_key=g7oEHBRcYKzCSiDBGD5DcLuoB7jF7tW7&limit=5").read())
+    # extract tenor GIF
+    for GIFs in top_5gifs['results']:
+        tenor.append(GIFs['media'][0]['tinygif']['url'])
 
-# extract giphy GIF
-for GIFs in data['data']:
-    giphy.append(GIFs['images']['original']['url'])
+    #giphy api
+    cat_process = cat.replace(' ', '+')
+    url = "http://api.giphy.com/v1/gifs/search?q=" + cat_process + "&api_key=g7oEHBRcYKzCSiDBGD5DcLuoB7jF7tW7&limit=5"
+    data = json.loads(urllib.request.urlopen(url).read())
 
-def get_gif_url (name):
+    # extract giphy GIF
+    for GIFs in data['data']:
+        giphy.append(GIFs['images']['original']['url'])
+
     if name=='tenor':
         return tenor
     else:
